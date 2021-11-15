7baa6276152a1cd438fc0e105b283c7c95cc9a14
Eddoru <91669196+Eddoru@users.noreply.github.com>
2021-11-15 05:40:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, a=1, b=1,c=0,d=0;
+
+    cin >> n;
+
+    if (n>=2) {
+        cout << b << endl;
+        for (a = 2; a <= n; a++) {
+            d = c + b;
+            cout << d << endl;
+            b = c;
+            c = d;
+        }
+    }else{
+        cout << "errore" << endl;
+
+
+
+    }
+return 0;
+
 }
